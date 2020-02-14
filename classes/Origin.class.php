73c802440682e5be781bc63622b1ca8ff096675d
Eugene Dv <vakuka@gmail.com>
2020-02-14 19:26:00 +0100

    Update DB Schema for Origin

--- classes/Origin.class.php
+++ classes/Origin.class.php
@@ -17,20 +17,19 @@ class Origin {
 	public function getAll() {
 
 		$locations = array();
-		$result = $this->db->query("SELECT l.LocationsID, l.City, p.ProvinceName 
+		$result = $this->db->select("SELECT l.LocationsID, l.City, p.ProvinceName 
 									FROM Locations AS l, Provinces AS p 
 									WHERE l.ProvincesID = p.ProvincesID
 									ORDER BY l.City");
-		if($result) {
-			while($row = mysql_fetch_assoc($result)) {
+		foreach ($rows as $row) {
 
-				$locations[] = array(
-					'Id' => $row['LocationsID'],
-					'city' => utf8_encode($row['City']),
-					'province' => utf8_encode($row['ProvinceName'])
-				);
-			}
+			$locations[] = array(
+				'Id' => $row['LocationsID'],
+				'city' => utf8_encode($row['City']),
+				'province' => utf8_encode($row['ProvinceName'])
+			);
 		}
+
 		return $locations;
 	}
 
@@ -40,28 +39,27 @@ class Origin {
 	public function getById($id) {
 
 		$location = array();
-		$result = $this->db->query("SELECT l.*, p.ProvinceName, p.ProvinceCode 
-									FROM Locations AS l, Provinces AS p
-									WHERE l.ProvincesID = p.ProvincesID
-									AND l.LocationsID = " . $id . "
-									LIMIT 1");
-
-		if($result) {
-			$row = $result->fetch_assoc();
-
-			$location['Id'] = $row['LocationsID'];
-			$location['Name'] = COMPANY_NAME;
-			$location['Company'] = COMPANY_NAME;
-			$location['StreetNumber'] = getStreetNumber($row['SteetAddress']);
-			$location['StreetName'] = getStreetName($row['SteetAddress']);
-			$location['City'] = $row['ActualCityName'];
-			$location['Province'] = $row['ProvinceCode'];
-			$location['Country'] = 'CA';
-			$location['PostalCode'] = $row['PostalCode'];
-			$location['PhoneAreaCode'] = getPhoneAreaCode($row['Phone']);
-			$location['Phone'] = getPhone($row['Phone']);
-			$location['LocationCode'] = $row['LocationCode'];
+		$result = $this->db->selectFirst("SELECT l.*, p.ProvinceName, p.ProvinceCode 
+										FROM Locations AS l, Provinces AS p
+										WHERE l.ProvincesID = p.ProvincesID
+										AND l.LocationsID = " . $id . "
+										LIMIT 1");
+		if(!count($row)){
+			return array();
 		}
+
+		$location['Id'] = $row['LocationsID'];
+		$location['Name'] = COMPANY_NAME;
+		$location['Company'] = COMPANY_NAME;
+		$location['StreetNumber'] = Address::getStreetNumber($row['SteetAddress']);
+		$location['StreetName'] = Address::getStreetName($row['SteetAddress']);
+		$location['PhoneAreaCode'] = Address::getPhoneAreaCode($row['Phone']);
+		$location['Phone'] = Address::getPhone($row['Phone']);
+		$location['City'] = $row['City'];
+		$location['Province'] = $row['ProvinceCode'];
+		$location['Country'] = 'CA';
+		$location['PostalCode'] = $row['PostalCode'];
+		$location['LocationCode'] = $row['LocationCode'];
 
 	    //Encode everything to UTF8
 		foreach ($location as &$val) {
@@ -75,30 +73,29 @@ class Origin {
 	public function getByOrderId($id) {
 
 		$location = array();
-		$result = $this->db->query("SELECT l.*, p.ProvinceName, p.ProvinceCode 
-									FROM Locations AS l, Provinces AS p, TrackingInfo AS t
-									WHERE l.ProvincesID = p.ProvincesID
-									AND t.TrackingCarrierID = 2 
-									AND l.LocationCode = t.LocationCode
-									AND t.OrderID = " . $id . "
-									LIMIT 1");
-
-		if($result) {
-			$row = $result->fetch_assoc();
-
-			$location['Id'] = $row['LocationsID'];
-			$location['Name'] = COMPANY_NAME;
-			$location['Company'] = COMPANY_NAME;
-			$location['StreetNumber'] = getStreetNumber($row['SteetAddress']);
-			$location['StreetName'] = getStreetName($row['SteetAddress']);
-			$location['City'] = $row['ActualCityName'];
-			$location['Province'] = $row['ProvinceCode'];
-			$location['Country'] = 'CA';
-			$location['PostalCode'] = $row['PostalCode'];
-			$location['PhoneAreaCode'] = getPhoneAreaCode($row['Phone']);
-			$location['Phone'] = getPhone($row['Phone']);
-			$location['LocationCode'] = $row['LocationCode'];
+		$result = $this->db->selectFirst("SELECT l.*, p.ProvinceName, p.ProvinceCode 
+								FROM Locations AS l, Provinces AS p, TrackingInfo AS t
+								WHERE l.ProvincesID = p.ProvincesID
+								AND l.LocationsID = t.LocationsID
+								AND t.OrderID = " . $id . "
+								LIMIT 1");
+		if(!count($row)){
+			return array();
 		}
+
+		$location['Id'] = $row['LocationsID'];
+		$location['City'] = $row['City'];
+		$location['Name'] = COMPANY_NAME;
+		$location['Company'] = COMPANY_NAME;
+		$location['StreetNumber'] = Address::getStreetNumber($row['SteetAddress']);
+		$location['StreetName'] = Address::getStreetName($row['SteetAddress']);
+		$location['Province'] = $row['ProvinceCode'];
+		$location['Country'] = 'CA';
+		$location['PostalCode'] = $row['PostalCode'];
+		$location['PhoneAreaCode'] = Address::getPhoneAreaCode($row['Phone']);
+		$location['Phone'] = Address::getPhone($row['Phone']);
+		$location['LocationCode'] = $row['StoreName'];
+
 
 	    //Encode everything to UTF8
 		foreach ($location as &$val) {
