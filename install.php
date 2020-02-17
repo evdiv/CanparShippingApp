3ad5b21fdef3f470bd8bc9fdfe971f239a59914a
Eugene Dv <vakuka@gmail.com>
2020-02-17 19:18:00 +0100

    Change Database Schema

--- install.php
+++ install.php
@@ -3,36 +3,26 @@
 require_once "./config.php";
 
 $db = new Canpar\Database();
-
-
-$db->query("CREATE TABLE Admin (
-							AdminID INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-							Username VARCHAR(150) NOT NULL,
-							Email VARCHAR(100) NOT NULL,
-							LocationsID INT(5)  NULL DEFAULT NULL,
-							ShippingAccess INT(1)  NULL DEFAULT NULL,
-							DateAdded TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP)"
-						);
-$db->query( "INSERT INTO Admin (Username, Email, LocationsID, ShippingAccess) VALUES ('John', 'john@yourdomain.com', '1', '1'");
-$db->query( "INSERT INTO Admin (Username, Email, LocationsID, ShippingAccess) VALUES ('Ken', 'ken@yourdomain.com', '1', '1'");        
+      
 $db->query("CREATE TABLE Accounts (
 							AccountsID INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 							FirstName VARCHAR(150) NOT NULL,
 							LastName VARCHAR(150) NOT NULL,
-							Apt VARCHAR(150) NULL DEFAULT NULL,
+							AptUnit VARCHAR(150) NULL DEFAULT NULL,
+							HomePhone VARCHAR(150) NULL DEFAULT NULL,
 							HomeAddress VARCHAR(255) NOT NULL,
 							HomeCity VARCHAR(255) NOT NULL,
 							ProvincesID INT(11)  NOT NULL,
 							PostalCode VARCHAR(255) NOT NULL,
-							Email VARCHAR(255) NOT NULL,
-							DateAdded TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP)"
+							Email VARCHAR(255) NOT NULL)"
 						);
-$db->query( "INSERT INTO Accounts (FirstName, LastName, Apt, HomeAddress, HomeCity, ProvincesID, PostalCode, Email) 
-	VALUES ('John', 'Scott', 32, '2 Camborne Crt.', 'Markham', 1, 'L3R7S3', 'johnscott@youdomain.com'");  
-$db->query( "INSERT INTO Accounts (FirstName, LastName, Apt, HomeAddress, HomeCity, ProvincesID, PostalCode, Email) 
-	VALUES ('Don', 'Wilson', 4, '59 Bell Farm Rd.', 'Barrie', 1, 'L4M5G1', 'donwilson@youdomain.com'");  
-$db->query( "INSERT INTO Accounts (FirstName, LastName, Apt, HomeAddress, HomeCity, ProvincesID, PostalCode, Email) 
-	VALUES ('Monica', 'Banman', 711, '3873 Harold Crescent', 'Sudbury', 1, 'P3N1J2', 'monicabanman@youdomain.com'");  
+$db->query( "INSERT INTO Accounts (FirstName, LastName, AptUnit, HomePhone, HomeAddress, HomeCity, ProvincesID, PostalCode, Email) 
+	VALUES ('John', 'Scott', 32, '(416)888-88-88', 2 Camborne Crt.', 'Markham', 1, 'L3R7S3', 'johnscott@youdomain.com'");  
+$db->query( "INSERT INTO Accounts (FirstName, LastName, AptUnit, HomePhone, HomeAddress, HomeCity, ProvincesID, PostalCode, Email) 
+	VALUES ('Don', 'Wilson', 4, '(416)999-99-99','59 Bell Farm Rd.', 'Barrie', 1, 'L4M5G1', 'donwilson@youdomain.com'");  
+$db->query( "INSERT INTO Accounts (FirstName, LastName, AptUnit, HomePhone, HomeAddress, HomeCity, ProvincesID, PostalCode, Email) 
+	VALUES ('Monica', 'Banman', 711, '(416)555-55-55','3873 Harold Crescent', 'Sudbury', 1, 'P3N1J2', 'monicabanman@youdomain.com'");  
+
 $db->query("CREATE TABLE ProductsBoxes (
 							ProductsBoxesID INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 							Description VARCHAR(250) NULL DEFAULT NULL,
@@ -47,14 +37,15 @@ $db->query( "INSERT INTO ProductsBoxes (Description, WeightLimit, BoxWeight, Len
 $db->query( "INSERT INTO ProductsBoxes (Description, WeightLimit, BoxWeight, Length, Width, Height) 
 					VALUES ('Medium box', 10.00, 0.40, 1.90, 2.20, 2.10");   
 $db->query( "INSERT INTO ProductsBoxes (Description, WeightLimit, BoxWeight, Length, Width, Height) 
-					VALUES ('Large box', 15.00, 0.90, 2.20, 2.80, 2.20"); 					 					   
+					VALUES ('Large box', 15.00, 0.90, 2.20, 2.80, 2.20"); 		
+
 $db->query("CREATE TABLE TrackingInfo (
 							TrackingInfoID INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 							OrderID INT(11)  NOT NULL,
-							AdminID INT(11)  NULL DEFAULT 1, 
+							LocationID INT(11)  NOT NULL,
 							TrackingCarrierID VARCHAR(250)  NULL DEFAULT 1,
 							TrackingCode VARCHAR(250) NULL DEFAULT NULL,
-							LocationCode VARCHAR(10) NULL DEFAULT NULL,
+							TrackingIdentifier VARCHAR(100) NULL DEFAULT NULL,
 							CourierService VARCHAR(100) NULL DEFAULT NULL,
 							Status INT(1)  NULL DEFAULT NULL,
 							Void INT(1)  NULL DEFAULT NULL,
@@ -64,15 +55,16 @@ $db->query("CREATE TABLE TrackingInfo (
 							Weight NUMERIC(10,2)  NULL DEFAULT NULL,
 							Reference VARCHAR(250) NULL DEFAULT NULL,
 							Note VARCHAR(512) NULL DEFAULT NULL,
+							Label VARCHAR(250) NULL DEFAULT NULL,
+							ManifestID VARCHAR(100) NULL DEFAULT NULL,
 							DateAdded TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP)"
 						);
 $db->query("CREATE TABLE TrackingReturnsInfo (
 							TrackingReturnsInfoID INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 							OrderID INT(11)  NOT NULL,
-							AdminID INT(11)  NULL DEFAULT 1, 
+							LocationID INT(11)  NOT NULL,
 							TrackingCarrierID VARCHAR(250)  NULL DEFAULT 1,
 							TrackingCode VARCHAR(250) NULL DEFAULT NULL,
-							LocationCode VARCHAR(10) NULL DEFAULT NULL,
 							CourierService VARCHAR(100) NULL DEFAULT NULL,
 							Length NUMERIC(10,2)  NULL DEFAULT NULL,
 							Width NUMERIC(10,2)  NULL DEFAULT NULL,
@@ -85,45 +77,52 @@ $db->query("CREATE TABLE TrackingReturnsInfo (
 $db->query("CREATE TABLE Orders (
 							OrdersID INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 							AccountsID INT(11)  NOT NULL,
+							Active INT(1)  NULL DEFAULT NULL,
+							ShippingName VARCHAR(250) NULL DEFAULT NULL,
+							CourierSelected VARCHAR(100) NULL DEFAULT NULL,
+							CourierService VARCHAR(100) NULL DEFAULT NULL,
+							TransAmount NUMERIC(10,2)  NULL DEFAULT NULL,
 							OrderDate TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP)"
 						);
-$db->query( "INSERT INTO Orders (AccountsID) VALUES ('1'");  
-$db->query( "INSERT INTO Orders (AccountsID) VALUES ('2'");    
-$db->query( "INSERT INTO Orders (AccountsID) VALUES ('1'");   
-$db->query( "INSERT INTO Orders (AccountsID) VALUES ('1'");   
-$db->query( "INSERT INTO Orders (AccountsID) VALUES ('2'");   
-$db->query("CREATE TABLE OrdersNotes (
-							OrderNotesID INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-							OrderID INT(11)  NOT NULL,
-							AdminID INT(11)  NULL DEFAULT 1, 
-							Note VARCHAR(512) NULL DEFAULT NULL,
-							NoteDate TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP)"
-						);
+
+$db->query( "INSERT INTO Orders (AccountsID, Active, CourierSelected, CourierService, TransAmount) 
+				VALUES ('1', 1, 'Purolator', 'Ground', 100.99");  
+
+$db->query( "INSERT INTO Orders (AccountsID, Active, CourierSelected, CourierService, TransAmount) 
+				VALUES ('2', 1, 'Purolator', 'Ground', 300.99");  
+
+$db->query( "INSERT INTO Orders (AccountsID, Active, CourierSelected, CourierService, TransAmount) 
+				VALUES ('3', 1, 'CanadaPost', 'Express', 200.99");  
+
+
 $db->query("CREATE TABLE Locations (
 							LocationsID INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 							City VARCHAR(255) NULL DEFAULT NULL,
+							StoreName VARCHAR(255) NULL DEFAULT NULL,
+							ContactName VARCHAR(255) NULL DEFAULT NULL,
 							ProvincesID INT(11)  NOT NULL,
 							Phone VARCHAR(50) NULL DEFAULT NULL,
-							Fax VARCHAR(50) NULL DEFAULT NULL,
 							Email VARCHAR(100) NULL DEFAULT NULL,
 							SteetAddress VARCHAR(255) NULL DEFAULT NULL,
-							PostalCode VARCHAR(255) NULL DEFAULT NULL,
-							Active INT(1)  NULL DEFAULT NULL,
-							Note VARCHAR(512) NULL DEFAULT NULL,
-							DateAdded TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP)"
-						);
-$db->query( "INSERT INTO Locations (City, ProvincesID, Phone, Email, SteetAddress, PostalCode) 
-	VALUES ('Toronto', '1', '416-999-99-99', 'toronto@yourdomain.com', '120 Torresdale Avenue', 'M2R3N7'");   
-$db->query( "INSERT INTO Locations (City, ProvincesID, Phone, Email, SteetAddress, PostalCode) 
-	VALUES ('Pickering', '1', '416-555-55-55', 'pickering@yourdomain.com', '188 Kingston Rd', 'L1V1C9'");
-$db->query( "INSERT INTO Locations (City, ProvincesID, Phone, Email, SteetAddress, PostalCode) 
-	VALUES ('Markham', '1', '416-444-44-44', 'markham@yourdomain.com', '9832 Markham Rd', 'L6E0E5'");
-$db->query( "INSERT INTO Locations (City, ProvincesID, Phone, Email, SteetAddress, PostalCode) 
-	VALUES ('Winnipeg', '2', '416-222-22-22', 'winnipeg@yourdomain.com', '1841 Pembina Highway', 'R3T2G6'");
+							PostalCode VARCHAR(255) NULL DEFAULT NULL");
+
+$db->query( "INSERT INTO Locations (City, StoreName, ContactName, ProvincesID, Phone, Email, SteetAddress, PostalCode) 
+	VALUES ('Toronto', 'Main Store', 'Alex Brown', '1', '416-999-99-99', 'toronto@yourdomain.com', '120 Torresdale Avenue', 'M2R3N7'"); 
+$db->query( "INSERT INTO Locations (City, StoreName, ContactName, ProvincesID, Phone, Email, SteetAddress, PostalCode) 
+	VALUES ('Pickering', 'Pickering Store', 'Kyle Ford', '1', '416-555-55-55', 'pickering@yourdomain.com', '188 Kingston Rd', 'L1V1C9'");
+$db->query( "INSERT INTO Locations (City, StoreName, ContactName, ProvincesID, Phone, Email, SteetAddress, PostalCode) 
+	VALUES ('Markham', 'Markham Store', 'John Doe', '1', '416-444-44-44', 'markham@yourdomain.com', '9832 Markham Rd', 'L6E0E5'");
+$db->query( "INSERT INTO Locations (City, StoreName, ContactName, ProvincesID, Phone, Email, SteetAddress, PostalCode) 
+	VALUES ('Winnipeg', 'Winnipeg Store', 'Anna Smith', '2', '416-222-22-22', 'winnipeg@yourdomain.com', '1841 Pembina Highway', 'R3T2G6'");
+
 $db->query("CREATE TABLE Provinces (
 							ProvincesID INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 							ProvinceName VARCHAR(100) NULL DEFAULT NULL,
 							ProvinceCode VARCHAR(2) NULL DEFAULT NULL)"
 						);
-$db->query( "INSERT INTO Provinces (ProvinceName, ProvinceCode) VALUES ('Ontario', 'ON'");
+$db->query( "INSERT INTO Provinces (ProvinceName, ProvinceCode) VALUES ('Alberta', 'AB'");
+$db->query( "INSERT INTO Provinces (ProvinceName, ProvinceCode) VALUES ('British Columbia', 'BC'");
 $db->query( "INSERT INTO Provinces (ProvinceName, ProvinceCode) VALUES ('Manitoba', 'MB'");
+$db->query( "INSERT INTO Provinces (ProvinceName, ProvinceCode) VALUES ('New Brunswick', 'NB'");
+$db->query( "INSERT INTO Provinces (ProvinceName, ProvinceCode) VALUES ('Ontario', 'ON'");
+$db->query( "INSERT INTO Provinces (ProvinceName, ProvinceCode) VALUES ('Quebec', 'QC'");
